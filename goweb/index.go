2f78353267903bb6a87f20aeb4196b5109ba1541
Rz_utomo <utomorezeki@gmail.com>
2018-10-29 20:47:26 +0700

    web and database fully functional

--- goweb/index.go
+++ goweb/index.go
@@ -81,30 +81,6 @@ func (p *AddCurrPage) checkExist(dbFrom string, dbTo string) bool {
 	return (checkFrom && checkTo)
 }
 
-func show() {
-	db, error := sql.Open("mysql","root:pass@tcp(sql1:3306)/CurrencyConv")
-	if error != nil {
-		panic(error)
-	}	
-	defer db.Close()
-
-	data, error := db.Query("SELECT * FROM Currency")
-	if error != nil {
-		log.Fatal(error)
-	}
-	defer data.Close()
-	for data.Next() {
-		var (
-			id   string
-			name string
-		)
-		if err := data.Scan(&id, &name); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("id %s name is %s\n", id, name)
-	}
-}
-
 func sevenDay(fromIn string, toIn string, timeIn time.Time, db *sql.DB) (float32,float32,*map[string]float32){
 	var (
 		sevenDR map[string]float32 = make(map[string]float32)
@@ -146,20 +122,98 @@ func sevenDay(fromIn string, toIn string, timeIn time.Time, db *sql.DB) (float32
 	return avg,max - min,&sevenDR
 }
 
+func sevenMR(fromIn string, toIn string, db *sql.DB) (float32,float32,*map[string]float32){
+	var (
+		sevenDR map[string]float32 = make(map[string]float32)
+		avg float32 = 0
+		max float32 = 0
+		min float32 = math.MaxFloat32
+	)
+	dataQuery := fmt.Sprintf("SELECT * FROM ExcData WHERE FromC='%s' AND ToC='%s'", fromIn,toIn)
+	data, error := db.Query(dataQuery)
+	if error != nil {
+		log.Fatal(error)
+	}
+	defer data.Close()
+	for data.Next() && len(sevenDR) < 7 {
+		data2, error := db.Query(dataQuery)
+		if error != nil {
+			log.Fatal(error)
+		}
+		defer data2.Close()
+		var maxRate float32
+		maxDate, _ := time.Parse(shortForm, "0001-01-01")
+		for data2.Next() {
+			var (
+				from   string
+				to string
+				date string
+				rate float32
+			)
+			if err := data2.Scan(&from, &to,&date, &rate); err != nil {
+				log.Fatal(err)
+			}
+			currDate, _ := time.Parse(shortForm, date) 
+			if _, exist := sevenDR[currDate.String()[:10]];currDate.After(maxDate) && !exist{
+				maxDate = currDate
+				maxRate = rate
+			}
+		}
+		sevenDR[maxDate.String()[:10]] = maxRate
+	}
+
+	for _, rateVal := range sevenDR {
+		avg = avg + rateVal
+		if rateVal > max { 
+			max = rateVal
+		}
+		if rateVal < min {
+			min = rateVal
+		}
+	}
+	avg = avg / float32(len(sevenDR))
+	return avg,max - min,&sevenDR
+}
+
 func addCurrHandler(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("addCurr.html")
     t.Execute(w, nil)
 }
 
 func addCurrReactHandler(w http.ResponseWriter, r *http.Request) {
+	db, error := sql.Open("mysql","root:pass@tcp(sql1:3306)/CurrencyConv")
+	if error != nil {
+		panic(error)
+	}	
+	defer db.Close()
+	data, error := db.Query("SELECT * FROM Currency")
+	if error != nil {
+		log.Fatal(error)
+	}
+	defer data.Close()
+	var placeHold bytes.Buffer
+	for data.Next() {
+		var (
+			from string
+			to string
+		)
+		if err := data.Scan(&from, &to); err != nil {
+			log.Fatal(err)
+		}
+		placeHold.WriteString(fmt.Sprintf("<tr><td>%s</td><td>%s</td></tr>", from,to))
+	}
+	page := &PageWriter{Data:template.HTML(placeHold.String())}
+
 	from := r.FormValue("from")
 	to := r.FormValue("to")
 	p := &AddCurrPage{fromIn: from, toIn: to}
 	if p.save() {
-		fmt.Fprintf(w, "Success currency input")
+		page.Title = fmt.Sprintf("Successfully added Currency %s -> %s",from,to)
 	} else {
-		fmt.Fprintf(w, "Fail to input! currency already exist")
+		page.Title = "Fail to add! Currency Exists in the DB"
 	}
+	t, _ := template.ParseFiles("addCurrReact.html")
+    t.Execute(w, page)
 }
 
 func dailyExHandler(w http.ResponseWriter, r *http.Request) {
@@ -177,8 +231,16 @@ func dailyExReactHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "rate must be numerical")
 		return
 	}
+
+	_, err = time.Parse(shortForm, date)
+	if err != nil{
+		fmt.Fprintf(w, fmt.Sprintf("error", err))
+		return
+	} 
 	p := &AddCurrPage{from,to,date,float32(rate)}
 	p.save()
+	t, _ := template.ParseFiles("dailyExReact.html")
+    t.Execute(w, nil)
 }
 
 func dateShowHandler(w http.ResponseWriter, r *http.Request) {
@@ -234,21 +296,15 @@ func sevenDaysHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func sevenDaysReactHandler(w http.ResponseWriter, r *http.Request) {
-	date := r.FormValue("date")
 	from := r.FormValue("from")
 	to := r.FormValue("to")
-	parsedTime, err := time.Parse(shortForm, date)
-	if err != nil{
-		fmt.Fprintf(w, fmt.Sprintf("error", err))
-		return
-	} 
 
 	db, error := sql.Open("mysql","root:pass@tcp(sql1:3306)/CurrencyConv")
 	if error != nil {
 		panic(error)
 	}	
 	defer db.Close()
-	avg,varia,sevenDR := sevenDay(from,to, parsedTime,db)
+	avg,varia,sevenDR := sevenMR(from,to,db)
 
 	var placeHold bytes.Buffer
 	for dateKey, rateVal := range *sevenDR {
@@ -257,6 +313,56 @@ func sevenDaysReactHandler(w http.ResponseWriter, r *http.Request) {
 	page := &PageWriter{Title: fmt.Sprintf("From %s --> To %s",from,to), Data:template.HTML(placeHold.String()),Average: avg,Variance:varia}
 	t, _ := template.ParseFiles("sevenDaysReact.html")
 	t.Execute(w, page)
+}
+
+func stopHandler(w http.ResponseWriter, r *http.Request) {
+	db, error := sql.Open("mysql","root:pass@tcp(sql1:3306)/CurrencyConv")
+	if error != nil {
+		panic(error)
+	}	
+	defer db.Close()
+	data, error := db.Query("SELECT * FROM Currency")
+	if error != nil {
+		log.Fatal(error)
+	}
+	defer data.Close()
+	var placeHold bytes.Buffer
+	for data.Next() {
+		var (
+			from string
+			to string
+		)
+		if err := data.Scan(&from, &to); err != nil {
+			log.Fatal(err)
+		}
+		placeHold.WriteString(fmt.Sprintf("<tr><td>%s</td><td>%s</td></tr>", from,to))
+	}
+	page := &PageWriter{Data:template.HTML(placeHold.String())}
+	t, _ := template.ParseFiles("stop.html")
+    t.Execute(w, page)
+}
+
+func stopReactHandler(w http.ResponseWriter, r *http.Request) {
+	from := r.FormValue("from")
+	to := r.FormValue("to")
+	
+	db, error := sql.Open("mysql","root:pass@tcp(sql1:3306)/CurrencyConv")
+	if error != nil {
+		panic(error)
+	}	
+	defer db.Close()
+	stringQuery := fmt.Sprintf("DELETE FROM Currency WHERE (FromC='%s' OR FromC='%s') AND (ToC='%s' OR ToC='%s')",from,to,from,to)
+	data, error := db.Query(stringQuery)
+	if error != nil {
+		log.Fatal(error)
+	}
+	stringQuery = fmt.Sprintf("DELETE FROM ExcData WHERE (FromC='%s' OR FromC='%s') AND (ToC='%s' OR ToC='%s')",from,to,from,to)
+	data, error = db.Query(stringQuery)
+	if error != nil {
+		log.Fatal(error)
+	}
+	defer data.Close()
+	http.Redirect(w,r,"/stop/form", http.StatusFound)
 }
 
 func main() {
@@ -268,5 +374,7 @@ func main() {
 	http.HandleFunc("/dateShow/react", dateShowReactHandler)
 	http.HandleFunc("/sevenDays/form", sevenDaysHandler)
 	http.HandleFunc("/sevenDays/react", sevenDaysReactHandler)
+	http.HandleFunc("/stop/form", stopHandler)
+	http.HandleFunc("/stop/react", stopReactHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
